f9cff2843b50fd28b5f2e41d59929929746aedac
Unknown <toyraynei@gmail.com>
2018-10-05 20:14:05 +0800

    second

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -7,7 +7,7 @@ void testGetTriangleType() {
 
 int main() {
     assert(addNumber(5, 2) == 7);
-    assert(subNumber(5, 2) == 3)
+    assert(subNumber(5, 3) == 2);
     testGetTriangleType();
     return 0;
 }
