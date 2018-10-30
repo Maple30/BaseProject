7c36239a4a7a72ca31a2c1cc7e6a7601315c8ee3
WU YI-TSE <yitsewu1998@gmail.com>
2018-10-30 14:39:51 +0800

    D0618990_整理檔案

--- src/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-int addNumber(int a, int b);
-int subNumber(int a, int b);
-int getTriangleType(int a, int b, int c);
-
-#endif // MAIN_H_INCLUDED
