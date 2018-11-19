5f6f3fef7ed6a62f06545ae934f1c276f46deee9
D0641771 <reunion41425252@gmail.com>
2018-11-20 00:39:48 +0800

    2018/11/20 00:39

--- web/Aboriginal/Aboriginal/urls.py
+++ web/Aboriginal/Aboriginal/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url,include
 from omniscient import views
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^', include('omniscient.urls')),
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
