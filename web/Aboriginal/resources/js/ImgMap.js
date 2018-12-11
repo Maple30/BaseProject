f6028b71ab42ef89439bdcc528e9c226c812a0e7
D0641771 <reunion41425252@gmail.com>
2018-12-11 21:08:09 +0800

    刪除功能被救回來了

--- web/Aboriginal/resources/js/ImgMap.js
+++ web/Aboriginal/resources/js/ImgMap.js
@@ -188,7 +188,6 @@ $(document).ready(function () {
 
 	//HS觀光景點
 	$('.HS1').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg40.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('臺北市');
 		$p1.stop().text('臺北市是臺灣最大的都市。也是政治、經濟、文化的中心。臺北的整體發展相當活潑多元，充滿朝氣。建築形式相當豐富，現代的、古代的都有。因此，喜歡古蹟的，不可錯過大稻埕的迪化街、艋舺的龍山寺，可以欣賞老師傅們的精雕細鑿，匠心美塑。喜歡古物的，不可錯過五千年文化珍藏的寶庫--故宮博物院、歷史博物館。');
@@ -196,7 +195,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001090'> </a></p>");
 	});
 	$('.HS2').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('基隆市');
 		$p1.stop().text('基隆位於臺灣北部，三面環山，僅北面一處有少量平原迎向大海，即為深水谷灣之基隆港。 港灣深入市區，水面寬闊，集商港、軍港、漁港於一身，為進入北臺灣門戶。');
@@ -204,7 +202,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001105' > </a></p>");
 	});
 	$('.HS3').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg41.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('新北市');
 		$p1.stop().text('新北市是臺灣最北的都市，環繞整個臺北市，總面積達二千多平方公里，海岸線長達120餘公里，幅員遼闊，新北市的東北、北部、西北部均臨海，市內北海岸與東北角海岸擁有豐富的海景奇觀及海岸風光，每逢夏日，海上盡是弄潮張帆的人潮，整條綿延的山海線，銜接成臺北人最鍾愛的黃金旅遊路線。');
@@ -212,7 +209,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001091' > </a></p>");
 	});
 	$('.HS4').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg42.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('桃園市');
 		$p1.stop().text('桃園市最早稱為芝芭里，因以前種有許多桃樹，盛產桃花的地方，而稱之為桃園，但目前桃花樹已不多見，早期曾因遠東第一大水庫－石門水庫的興建而名盛一時，許多觀光客到桃園，勢必要到石門水庫。而境內最重要的地點，莫過於國家的大門-臺灣桃園國際機場，每年利用桃園國際機場出入境旅客更達千餘萬人次，桃園市因此也成為「國門之都」。 ');
@@ -220,7 +216,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001107'  > </a></p>");
 	});
 	$('.HS5').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg43.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('新竹縣');
 		$p1.stop().text('新竹縣是全臺客家族群分佈最多的縣，早期多由紅毛港及南寮港登陸，以海豐和陸豐人居多。居住在風城的客家人，因為環境的關係，無論是語言、風俗民情和信仰，均自成一格。');
@@ -228,7 +223,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001108'  > </a></p>");
 	});
 	$('.HS6').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg44.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('新竹市');
 		$p1.stop().text('新竹原名竹塹，北從桃園台地的尾端至鳳山崎，南到香山丘陵，三面環山，僅西面臨海，形成一畚箕地形。是臺灣與中國大陸東南沿海距離最短之處，人口分佈以客家人居多，占總人口數的85％以上，因此在臺灣的客家文化史上扮演舉足輕重的角色，而閩南人則多係來自泉州，現集中於新竹市。');
@@ -236,7 +230,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001109'  > </a></p>");
 	});
 	$('.HS7').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg45.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('苗栗縣');
 		$p1.stop().text('苗栗原名「貓裡」，語意為「平原」或「美麗」之意。位於中部蜂腰地帶，依山面海，群山夾峙的平原，是一個多山的縣城。客家人的入墾苗栗地區，始於乾隆年間，他們沿著後龍溪開發，深入山區，先後開發苗栗縣內之西南、西北及東部地區。由於縣境以客家人居多，道地客家風味的餐飲，頗具特色。');
@@ -244,7 +237,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001110' > </a></p>");
 	});
 	$('.HS8').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg46.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('臺中市');
 		$p1.stop().text('臺中位於臺灣西半部的樞紐位置，四季氣候宜人。日治時期，積極興建鐵路、開通公路及海港運輸，讓臺中遂躍身為中臺灣政治、經濟、交通、文化的重鎮。臺中市對古蹟的保存不遺餘力，完整保留了日治時期棋盤式街道、具有兩百年歷史的樂成宮、雕工華麗且香火鼎盛的城隍廟，以及三殿式格局的萬和宮、古蹟-張廖家廟，以及迄今同樣已有兩百多年歷史的大甲鎮瀾宮，在在都讓人發思古之幽情。 ');
@@ -252,7 +244,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001112'  > </a></p>");
 	});
 	$('.HS9').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg47.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('彰化縣');
 		$p1.stop().text('彰化素有「臺灣穀倉」之稱，東南北三面環山，西臨臺灣海峽，境內以八卦山為主峰。該區氣候暖和，雨量適中，宜於農業發展，特別以花卉栽培最為發達。彰化古稱「半線」，係原住民社名。清雍正年間，閩海居民相繼遷移，聚居現今鹿港一帶，人口、經貿日益繁榮，始設縣取「顯彰聖化」之意，而名「彰化」。');
@@ -260,7 +251,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001113' > </a></p>");
 	});
 	$('.HS10').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg48.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('雲林縣');
 		$p1.stop().text('雲林縣位於臺灣的中南部，嘉南平原的最北端，全縣幾乎是沖積平原，是臺灣的榖倉及農業縣，縣內農產品眾多，不論是聞名遐邇的斗六文旦、香Q有嚼勁的濁水米，還有沾醬不可少，香醇濃郁的西螺醬油，更或者是養生療效極佳的莿桐大蒜精，這些可都是雲林的好東西。境內天然景觀當屬草嶺風景區了，草嶺地區的天然景觀豐富，讓人不得不讚嘆大自然鬼斧神工的巧妙！');
@@ -269,7 +259,6 @@ $(document).ready(function () {
 	});
 
 	$('.HS11').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg49.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('南投縣');
 		$p1.stop().text('若說中部地區是臺灣的心臟地帶，那南投地區即是「臺灣心」。南投是臺灣唯一沒有臨海的縣。南投縣市面積 4100平方公里，境內山多平原少，四周盡是山巒疊翠，觀光資源相當豐富，可說隨處皆是風景。山間城集的埔里，這裡的水質甜美、美酒香醇醉人，有著說不盡的好味道。水里的蛇窯、集集的綠色隧道、溪頭、杉林溪、日月潭、九族文化村等都是觀光旅遊的好去處。');
@@ -277,7 +266,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001114'  > </a></p>");
 	});
 	$('.HS12').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg50.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('嘉義縣');
 		$p1.stop().text('嘉義原名諸羅，明天啟2（1622）年，福建漳州人顏思齊率人自笨港（今雲林北港與嘉義新港一帶）登陸開墾，可謂臺灣最古老的漢民聚落之一。清乾隆51（1786）年，林爽文起義，諸羅軍民全力守城，事後清廷為嘉許當地軍民之義舉，遂將諸羅改名為嘉義，至今嘉義市義民廟內尚保留此段史蹟。');
@@ -285,7 +273,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001116'  > </a></p>");
 	});
 	$('.HS14').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg51.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('嘉義市');
 		$p1.stop().text('位於臺灣西南部「嘉南平原」北端，北回歸線經過本市南邊約一公里處。本市開發甚早，荷蘭人留有「紅毛井」、「紅毛埤」等遺跡。明鄭時代，在屯田地設有營鎮，日漸形成聚落。清康熙43年（1704），縣署歸治諸羅山（嘉義市），並築建木柵城，是臺灣最早的城柵。');
@@ -293,7 +280,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001117'   > </a></p>");
 	});
 	$('.HS13').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg52.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('臺南市');
 		$p1.stop().text('臺南市是臺灣的發祥地，是全臺歷史最悠久的都市。西元1661年，鄭成功驅逐荷蘭人後，在臺南開府設治，當時中國居民移居來臺者紛至沓來，臺南都會之規模於是奠定。至1683年，清國平臺，在臺南設臺灣府，為全臺首府。1885年，臺灣建省，遂改臺灣府為臺南府，是為臺南得名之緣由。 ');
@@ -301,7 +287,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001119'  > </a></p>");
 	});
 	$('.HS15').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg53.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('高雄市');
 		$p1.stop().text('臺灣南方最繁華的國際城市─『高雄市』，為2009世界運動會主辦城市，因受海洋氣候調節，全年陽光普照、氣候宜人，有獨特的「海洋首都」特性。近年來大力推展觀光事業，已成為全臺最美麗的城市之一。 ');
@@ -309,7 +294,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001121'  > </a></p>");
 	});
 	$('.HS16').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg54.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('屏東縣 ');
 		$p1.stop().text('屏東縣是臺灣最南端的縣，輪廓狹長，依山傍海，東臨太平洋，西向臺灣海峽，南面巴士海峽，地處熱帶，四季如春，風光明媚，椰影婆娑，充滿南國風味，有「臺灣南洋」之稱，是國內不可多得的觀光聖地。');
@@ -317,7 +301,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001122'> </a></p>");
 	});
 	$('.HS17').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg55.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('宜蘭縣 ');
 		$p1.stop().text('宜蘭市位居蘭陽平原中心的精華地帶，河川縱橫提供豐富的灌溉資源，讓居於平原中央的宜蘭市擁有肥沃土地與充裕水源，成為一個農產豐饒的稻米之鄉。這麼多年來，宜蘭市一直是蘭陽平原的經政文教重心，也是南來北往的交通要道。');
@@ -325,7 +308,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001106' > </a></p>");
 	});
 	$('.HS18').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg56.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('花蓮縣');
 		$p1.stop().text('花蓮─昔日名「哆囉滿」，明朝弘治年間，葡萄牙人航海經過臺灣海峽，繞到臺灣東海岸，發現砂金，就用葡萄牙產金河流「利澳特愛魯」（Rio Dnero）稱呼花蓮。在漢人移居之前，原住民取名也有因自然位置與地形、特殊產物或發生事故而命名的，祇是漢人開拓花蓮之後，更有因拓殖建置的初景、歷史的動機、日本移民懷鄉思而命其本國地名、或語言轉訛而來的地名。');
@@ -333,7 +315,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001124' > </a></p>");
 	});
 	$('.HS20').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg58.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('連江縣');
 		$p1.stop().text('馬祖列島以「媽祖」而得名，位於臺灣海峽西北西方，隸屬於福建省連江縣，面臨閩江口、連江口及羅源灣，與大陸僅呎水之隔，轄分南竿、北竿、莒光及東引等4鄉5島，為目前台澎金馬自由地區最北端之領土。');
@@ -341,7 +322,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001127' > </a></p>");
 	});
 	$('.HS19').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg57.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('臺東縣');
 		$p1.stop().text('臺東縣為臺灣第三大縣，境內多山，西邊雄踞著中央山脈，中北部有海岸山脈盤據。從平原到高山，從海洋到離島，自然景觀豐富，從熱帶、溫熱帶到寒帶植物，你都可在臺東發現。由於受山脈阻隔與洋流影響，臺東縣年均溫在22度以上的月份高達8個月。所以臺東又有太陽的故鄉之稱。除了高山地形外，臺東海岸線綿延長達176公里，近年有賞鯨豚的生態旅遊活動，讓臺東的海洋之旅更為豐富。');
@@ -349,7 +329,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001123' > </a></p>");
 	});
 	$('.HS21').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg59.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('金門縣');
 		$p1.stop().text('金門，一個曾經在歷史上因為屢次勝利戰役而赫赫有名的自由聖地，是中華民國政府所轄的島縣，屏障著臺灣與澎湖，也是自由世界在太平洋上的反共前哨。');
@@ -357,7 +336,6 @@ $(document).ready(function () {
 		$p2.stop().append("<p class='morelink'>更多內容<a href='m1.aspx?sNo=0001126' > </a></p>");
 	});
 	$('.HS22').mouseover(function () {
-		// $backgroundImage.stop().css('background', 'url(/static/photo/index/map/bg60.jpg) center center');
 		$backgroundImage.stop().css('background-size');
 		$title.stop().text('澎湖縣');
 		$p1.stop().text('澎湖舊稱「平湖」、「澎瀛」、「澎海」，又稱「西瀛」、「島夷」等等，澎湖的建築多與福建、金門等地相似，材料則以當地出產的硓(石古)石、石灰、砂土、玄武岩為主。此地宗教活動特別頻繁，有關於媽祖、王爺與王船、鸞堂等極具地方色彩之活動，並有大小廟宇約近200座，較具名氣的有天后宮、城隍廟、武聖廟、保安宮等。');
