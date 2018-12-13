57ef269a84c2f12b3eee0b30d55a220acea240ab
D0641771 <reunion41425252@gmail.com>
2018-12-13 21:58:54 +0800

    正在寫註冊跟在首頁加上東西

--- web/Aboriginal/omniscient/forms.py
+++ web/Aboriginal/omniscient/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Works
+from .models import Issue
+from django.contrib.auth.models import User
 
 class WorksForm(forms.ModelForm):
     class Meta:
         model = Works
         fields = ('Title', 'Introduction', 'Contact', 'Photo')
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
